d369da87dcc5b60589598399a98e9a8ac0a25689
asadullahk-specs <asadullahk4u@gmail.com>
2026-08-09 11:55:58 +0500

    fix: update Vercel configuration with root api handler and monorepo install commands

---
+++ api/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const app = require('../server/src/app');
+const connectDB = require('../server/src/config/db');
+
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+};
