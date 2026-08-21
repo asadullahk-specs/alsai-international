0fdc00e1d1b8646cde2de993a7a7ae2c351ef328
Asadullah khan <asadullahk4u@gmail.com>
2026-08-21 13:45:22 +0500

    Update of changes

--- client/src/components/BrandMark.jsx
+++ client/src/components/BrandMark.jsx
@@ -1,7 +1,9 @@
-const BrandMark = ({ className = 'w-6 h-6', color = '#A9662A' }) => (
-  <svg viewBox="0 0 24 24" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 1L14 10L23 12L14 14L12 23L10 14L1 12L10 10L12 1Z" fill={color} />
-  </svg>
+const BrandMark = ({ className = 'w-6 h-6', alt = "AL SA'I Mark" }) => (
+  <img
+    src="/logo.png"
+    alt={alt}
+    className={`${className} object-contain inline-block`}
+  />
 );
 
 export default BrandMark;
